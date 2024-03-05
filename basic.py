8496d53b99e8c10458cee50f80d2ccbd5394849c
Brian McMahon <brian.mcmahon1@ucdconnect.ie>
2024-03-05 13:55:37 +0000

    asd

--- basic.py
+++ basic.py
@@ -6,7 +6,45 @@ from flask import url_for
 from flask import render_template
 import os
 from flask import send_file
-import sql_puller
+import mysql.connector
+import json
+
+
+def static_data():
+    print("Starting SQL Static Data Pull Package")
+    HOST = "dublinbikes.c1ywqa2sojjb.eu-west-1.rds.amazonaws.com"
+    USER = "admin"
+    PASSWORD = "boldlynavigatingnature"
+    DATABASE = "dublinbikes"
+
+
+    connection = mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DATABASE
+    )
+    cursor = connection.cursor()
+    print("Connection Made")
+    query = "SELECT * FROM dublinbikes.station;"
+    cursor.execute(query)
+
+    # Fetch all rows and convert to a list of dictionaries
+    weather = cursor.fetchall()
+    result = []
+
+    for row in weather:
+        d = {}
+        for i, col in enumerate(cursor.description):
+            d[col[0]] = row[i]
+
+        result.append(d)
+    print("finished")
+    # Convert the list of dictionaries to JSON and print it
+    json_result = json.dumps(result)
+    connection.close()
+
+    return json_result
 
 #print(os.listdir('.'))
 #print(sql_puller.static_data())
@@ -55,7 +93,7 @@ def get_json_data():
     try:
         #with open(json_path, 'r') as json_file:
         #    data = json.load(json_file)
-        data = sql_puller.static_data()
+        data = static_data()
         return data
     except Exception as e:
         return str(e), 500
