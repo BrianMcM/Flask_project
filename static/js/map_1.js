d0a46bc0b60b384aa79912d3ba7398336b940b62
Brian McMahon <brian.mcmahon1@ucdconnect.ie>
2024-04-02 16:59:46 +0100

    asd

--- static/js/map_1.js
+++ static/js/map_1.js
@@ -51,7 +51,7 @@ async function initMap() {
   // Request needed libraries.
   //@ts-ignore
   const { Map, InfoWindow } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
 
   // The map, centered at The Spire
   map = new Map(document.getElementById("map"), {
